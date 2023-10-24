2430bb33b04459f8375f0ca4d22b8d8ed6b0c654
Your Name <you@example.com>
2023-10-24 07:29:13 +0000

    1

--- test/resnet.py
+++ test/resnet.py
@@ -1,9 +1,9 @@
 import torch
-
+import welder
 import torch._dynamo as torchdynamo
 import torch._inductor as torchinductor
 
 model = torch.hub.load("pytorch/vision:v0.10.0", "resnet18", pretrained=True).cuda()
-opt_model = torch.compile(model, backend="inductor")
+opt_model = torch.compile(model, backend="welder")
 _input = torch.randn(1, 3, 64, 64).cuda()
 out = opt_model(_input)
--- welder/__init__.py
+++ welder/__init__.py
@@ -3,14 +3,14 @@ import torch
 
 from torch._inductor.lowering import lowerings
 
-black_list = ["tuned_mm", "tuned_bmm", "tuned_addmm"]
-to_erase = []
-for k in lowerings.keys():
-    if lowerings[k].__name__ in black_list:
-        to_erase.append(k)
-for e in to_erase:
-    del lowerings[e]
-from .triton_kernels import mm, bmm
+# black_list = ["tuned_mm", "tuned_bmm", "tuned_addmm"]
+# to_erase = []
+# for k in lowerings.keys():
+#    if lowerings[k].__name__ in black_list:
+#        to_erase.append(k)
+# for e in to_erase:
+#    del lowerings[e]
+# from .triton_kernels import mm, bmm
 
 from welder import compile
 from welder import graph
